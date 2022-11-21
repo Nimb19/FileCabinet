addd464b43222d0ab63788479c8875ff27da296e
Ганзин Юрий Михайлович <Yury.Ganzin@monitel.com>
2022-11-21 15:39:13 +0300

    1

--- FileCabinet/.NET/C#/_TestProjects/CsTests/UnitTests.cs
+++ FileCabinet/.NET/C#/_TestProjects/CsTests/UnitTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Concurrent;
 using System.Diagnostics;
 using System.Text;
 
@@ -9,9 +10,101 @@ public class UnitTests
     [TestMethod]
     public void test()
     {
+        var t = "sadasdas";
+        var obj = (object)t;
+
+        var ostr = obj as string;
+        Trace.WriteLine($"{ostr == null}");
+    }
+
+    public enum testen
+    {
+        one = 0x0001,
+        two = 0x0010,
+        three = 0x0100,
+        fo = 0x1000
+    }
+
+    [TestMethod]
+    public void testenum()
+    {
+        var test = testen.one | testen.two | testen.fo;
+        switch (test)
+        {
+            case testen.one: 
+                Trace.WriteLine("testen.one");
+                break;
+            case testen.two:
+                Trace.WriteLine("testen.two");
+                break;
+            case testen.three:
+                Trace.WriteLine("testen.three");
+                break;
+            case testen.fo:
+                Trace.WriteLine("testen.fo");
+                break;
+            default: 
+                Trace.WriteLine("def");
+                break;
+        }
+    }
+
+    [TestMethod]
+    public void testblocking()
+    {
+        var bc = new BlockingCollection<string>();
+        var task1 = Task.Run(() =>
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                bc.Add($"{i}");
+                Trace.WriteLine($"Отдал: {i}");
+
+                Task.Delay(500).Wait();
+            }
+        });
+        var task2 = Task.Run(() =>
+        {
+            var i = 0;
+            var chet = true;
+            const int max = 5;
+            var action = () =>
+            {
+                Trace.WriteLine($"Начал ждать");
+                foreach (var item in bc.GetConsumingEnumerable())
+                {
+                    var prishlo = $"Пришёл: {item}";
+                    Trace.WriteLine(prishlo);
+                    i++;
+
+                    if (i == max)
+                    {
+                        i = 0;
+                        chet = !chet;
+                        Trace.WriteLine($"5 штук готовы");
+                        //bc.CompleteAdding();
+                        if (chet)
+                        {
+                            Task.Delay(2000).Wait();
+                            Trace.WriteLine("Закрываюсь, чётно");
+                            return;
+                        }
+                    }
+                }
+            };
+            for (int g = 0; g < 3; g++)
+            {
+                action();
+            }
+        });
+        task2.Wait();
+    }
+
+    [TestMethod]
+    public void testintern()
+    {
         var str1 = "hello";
-        var pstr = "hell";
-        var str2 = pstr + "o";// new StringBuilder().Append("hell").Append("o").ToString();
+        var str2 = "hell" + "o";// new StringBuilder().Append("hell").Append("o").ToString();
         var oStr1 = (object)str1;
         var oStr2 = (object)str2;
         Trace.WriteLine(str1 == str2);
@@ -19,7 +112,8 @@ public class UnitTests
         Trace.WriteLine(str1 == oStr2);
 
         // https://www.youtube.com/watch?v=M32SEu0hY7w 56 min primerno
-        // только на этапе компиляции одни и те же строки (или одни и те же, но полученные в текущей строке) будут интернированы
+        // только на этапе компиляции одни и те же строки будут интернированы
+        // (или одни и те же, но полученные в текущей строке (pstr + "o" не считается за формирование в одной сроке, считается "hell" + "o"))
     }
 
     [TestMethod]
